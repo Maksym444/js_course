09414379f04fef86c254536b42e351a720bc0f6d
Maksym <makskryvenko@gmail.com>
2020-06-24 17:48:43 +0300

    Second attempt first home_work

--- home_work/src/index.js
+++ home_work/src/index.js
@@ -1,15 +1,20 @@
 // Your code here
+
 let age = prompt('Сколько Вам лет?');
 let year;
 let oneNumber = (age % 10);
 let twoNumber = (age % 100)
-    if ((4 < oneNumber && oneNumber <= 9) || (twoNumber >= 10 && twoNumber < 21)) {
+    if (4 < oneNumber && oneNumber <= 9 || (twoNumber >= 10 && twoNumber < 21)) {
         year = ('лет');
     }
     else if (oneNumber == 1) {
         year = ('год');
+        console.log (year)
     }
-    else {
+    else if (1 < oneNumber && oneNumber < 5)  {
         year = ('года');
     }
-    alert('Вам' + ' ' + age + ' ' + year + '!');
+    else {
+        year= ('лет')
+    }
+alert('Вам' + ' ' + age + ' ' + year + '!');
